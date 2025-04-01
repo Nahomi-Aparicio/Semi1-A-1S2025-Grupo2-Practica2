1dc03290bae59dd679a3a58439301d31bac0bc5b
Sebastián Godoy <3742753000101@ingenieria.usac.edu.gt>
2025-03-31 21:19:28 -0600

    feat: mysql db added

---
+++ DB/script.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE taskflow_cloud;
+
+USE taskflow_cloud;
+
+-- Tabla de usuarios
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(100) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL, -- Debe almacenarse encriptada
+    imagen_perfil_url TEXT
+);
+
+-- Tabla de tareas
+CREATE TABLE tareas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    completada BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+-- Tabla de archivos
+CREATE TABLE archivos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    nombre_archivo VARCHAR(255) NOT NULL,
+    tipo_archivo VARCHAR(50),
+    url_archivo TEXT NOT NULL,
+    fecha_subida DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
